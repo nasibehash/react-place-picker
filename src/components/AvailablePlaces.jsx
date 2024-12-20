881f51373d68f1a36c51526db6b30183c549ef40
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2024-12-20 17:07:47 +0330

    Feature: Use custom hook in two components

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,33 +1,27 @@
 import Places from './Places.jsx';
-import { useEffect, useState } from 'react';
 import { fetchingAvailablePlaces } from '../http.js';
 import { sortPlacesByDistance } from '../loc.js';
 import Error from './Error.jsx';
+import { useFetch } from '../hooks/useFetch.js';
+
+async function fetchSortedPlaces () {
+  const places = await fetchingAvailablePlaces();
+
+  return new Promise(( resolve ) => {
+    navigator.geolocation.getCurrentPosition(( position ) => {
+      const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces ( {onSelectPlace} ) {
-  const [ availablePlaces, setAvailablePlaces ] = useState([]);
-  const [ isFetching, setIsFetching ] = useState(false);
-  const [ error, setError ] = useState();
 
-  useEffect(() => {
-    async function fetchPlaces () {
-      setIsFetching(true);
-      try {
-        const places = await fetchingAvailablePlaces();
-
-        navigator.geolocation.getCurrentPosition(( position ) => {
-          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({message: error.message || 'error occured'});
-        setIsFetching(false);
-      }
-    }
-
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="Error" message={error.message}/>;
